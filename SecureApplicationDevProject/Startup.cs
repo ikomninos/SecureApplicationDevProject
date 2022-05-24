620dd85e0b19034ace135e2fcca78c24dcb40299
ioannis komninos <ikomninos@nyc.gr>
2022-05-24 18:48:59 +0300

    changes

--- SecureApplicationDevProject/Startup.cs
+++ SecureApplicationDevProject/Startup.cs
@@ -77,7 +77,7 @@ namespace SecureApplicationDevProject
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
-
+            
             
         }
 
